a8ff4ae252e1968129e07481c42615b424abe4c3
wai lok <lokcentral0613@gmail.com>
2016-03-27 02:42:09 +0800

    data setup

--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,6 @@ var baseFactory = function(model, $http) {
   var getJson = function(callback) {
     $http.get('data/'+model+'.json').success(callback);
   };
-
   return {
     all: function (callback) {
       return getJson(callback);
@@ -12,24 +11,9 @@ var baseFactory = function(model, $http) {
 
 angular.module('vi-pizza.services', [])
 
-.factory('Pizza', function($http) {
-  return baseFactory('pizza', $http);
-})
-
-.factory('PizzaSet', function($http) {
-  return baseFactory('pizzaSet', $http);
-})
-
-.factory('FoodFactory', function(Pizza, PizzaSet) {
+.factory('FoodFactory', function($http) {
   var createFood = function(foodType) {
-    switch(foodType) {
-      case 'pizza':
-        return Pizza;
-      case 'pizzaSet':
-        return PizzaSet;
-      default:
-        return 'PizzaHut';
-    }
+    return baseFactory(foodType, $http);
   }
   return {
     createFood: createFood
